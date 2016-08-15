7592c5a691595bdc14d056dd127e1fb878a2e3dd
Taeyoung Kim <Taeyoung Kim>
2016-08-15 20:36:49 +0900

    add sudo to gulp install

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" 
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 # install npm modules
-npm install -g gulp
+sudo npm install -g gulp
 # install letsencrypt
 sudo apt-get -y install git bc
 sudo git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
