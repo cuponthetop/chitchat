5667a774b0c522d22b1addbb9e5f4786f5c04440
Taeyoung Kim <Taeyoung Kim>
2016-08-15 22:30:25 +0900

    gave up local logging, save only sites file

--- start.sh
+++ start.sh
@@ -2,10 +2,15 @@ mkdir log
 mkdir log/nginx
 
 #start mongodb
-sudo service mongod start
+sudo service mongod restart
+
+# copy nginx config 
+sudo cp ./sites-available/chitchat.zone /etc/nginx/sites-available/
+sudo rm /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+sudo ln -s /etc/nginx/sites-available/chitchat.zone /etc/nginx/sites-enabled/
 
 # start nginx
-sudo nginx -c ~/chitchat/nginx/nginx.conf
+sudo service nginx restart
 
 # clone server repositories
 git clone https://github.com/cuponthetop/chitchat_front.git
