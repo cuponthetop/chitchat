d3bd1be558bf6fbef58045a8ddc816223e15d4e0
Taeyoung Kim <Taeyoung Kim>
2016-08-19 00:29:42 +0900

    change well-known path

--- init_letsencrypt.sh
+++ init_letsencrypt.sh
@@ -1,3 +1,3 @@
 
 cd /opt/letsencrypt
-./letsencrypt-auto certonly -a webroot --webroot-path=/home/ubuntu/chitchat/chitchat_front/public -d chitchat.zone -d www.chitchat.zone
+./letsencrypt-auto certonly -a webroot --webroot-path=/home/ubuntu/chitchat/chitchat_front/.well-known -d chitchat.zone -d www.chitchat.zone
